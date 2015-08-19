db8c4a7e4a464876ab8f0587b7dd15e1fe59a074
vaibhav <vaibhav.soundarkar@brsoftech.in>
2015-08-19 14:58:23 +0530

    New Class Added

---
+++ Demo/Demo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo
+{
+    public class Class1
+    {
+        public void SetDetails()
+        {
+
+        }
+    }
+}
